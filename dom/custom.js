3eb8f4d22e543ccdb7abc65dccdfd10b48c2de48
uhj1993 <dzzd777@naver.com>
2021-02-13 22:37:47 +0900

    문서: DOM을 이용한 끝말잇기 문서 추가
    dom을 연습하기 위해 끝말잇기 기능이 담긴 문서 추가

---
+++ dom/custom.js
@@ -0,0 +1,17 @@
+let firstWord = document.querySelector(".startWord");
+let innerText = document.querySelector(".text");
+let button = document.querySelector("button");
+let result = document.querySelector(".result");
+
+function callBack() {
+    if (firstWord.textContent[firstWord.textContent.length - 1] === innerText.value[0]) {
+        result.textContent = '딩동댕';
+        firstWord.textContent = innerText.value;
+        innerText.value = '';
+    } else {
+        result.textContent = '땡';
+        
+    }
+}
+
+button.addEventListener('click', callBack);
