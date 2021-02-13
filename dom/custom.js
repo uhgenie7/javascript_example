eea0acb742d758a9358bce419b39345e414eb43f
uhj1993 <dzzd777@naver.com>
2021-02-13 23:22:31 +0900

    버그: 유실된 데이터 유실된 데이터 복구

--- dom/custom.js
+++ dom/custom.js
@@ -2,7 +2,7 @@ let firstWord = document.querySelector(".startWord");
 let innerText = document.querySelector(".text");
 let button = document.querySelector("button");
 let result = document.querySelector(".result");
-
+ 
 function callBack() {
     if (firstWord.textContent[firstWord.textContent.length - 1] === innerText.value[0]) {
         result.textContent = '딩동댕';
