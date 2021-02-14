c5bc32f899aed9503c3745eaf1ab0492552fd821
uhj1993 <dzzd777@naver.com>
2021-02-14 21:00:38 +0900

    추가: else문 조건 추가 strke, ball은 0으로 초기화해주고, 입력한 답을 split()메서드를 이용해 문자열로 만든다. 반복문을 사용해 입력값과 제시된 값을 비교한다. 조건문을 사용해 같은 자리이면서 같은 값일 경우 strike 1 추가, 다른 자리이면서 같은 값이 존재할 경우 ball 1 추가한다. 반복문 이후 결과창을 띄워준다.

--- number_baseball/custom.js
+++ number_baseball/custom.js
@@ -27,9 +27,24 @@ form.addEventListener("submit", function (e) {
         input.focus();
     } else {
         console.log("틀렸어");
-        mainResult.innerText = "꽝";
+        let answerArray = answer.split('');
+        console.log(answerArray);
+        // 문자열이 찍힘.
+        let strike = 0;
+        let ball = 0;
+        for (let i = 0; i < 3; i++){
+            if (Number(answerArray[i]) === numArray[i]) {
+                strike += 1;
+            } else if (numArray.indexOf(Number(answerArray[i])) > -1){
+                ball += 1;
+            }
+        }
+        mainResult.innerText = `${strike}스트라이크 ${ball}볼입니다.`;
+        input.value = ""
+        input.focus();
     }
 })
 
 // 배열.join(구분자) -> 문자로 변환시켜줌
-// 문자.split(구분자) -> 배열로 바꿔줌.
+// 문자.split(구분자) -> 배열로 바꿔줌. 단 배열 안에 들어가는 것은 문자임.
+// 배열.indexOf(값) -> 값의 위치를 알 수 있다. 없으면 -1 출력.
